847068f6e1b7d349ec4a1c0cf6e881e380559646
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-06 22:26:49 -0400

    add RoomContext and NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import NavBar from "./components/NavBar"
+import RoomContext from "./contexts/RoomContext"
 
 function App() {
   return (
     <div className="App">
+      <RoomContext>
+        <NavBar/>
+      </RoomContext>
+    
+      
     </div>
   );
 }
