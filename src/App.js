c1eaa9e9e6a722664d2c0a8d49d3dde71d162138
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-08 21:32:51 -0400

    import Rent component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from "./components/NavBar"
+import Rent from './components/Rent';
 import RoomContext from "./contexts/RoomContext"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <RoomContext>
         <NavBar/>
+        <Rent/>
       </RoomContext>
     
       
