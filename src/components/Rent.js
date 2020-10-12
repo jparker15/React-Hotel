5132a277f0b75c317c319f85bf446ab43cc7476d
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-11 23:52:51 -0400

    idk

--- src/components/Rent.js
+++ src/components/Rent.js
@@ -1,14 +1,31 @@
-import React,{useContext} from 'react' 
+import React,{useContext, useState} from 'react' 
 import {RoomContext} from "../contexts/RoomContext"
 
 export default function Rent() {
 
-    const {rooms} = useContext(RoomContext)
+    const {rooms} = useContext(RoomContext);
+    const [name,setName] = useState("");
+
+
+    const demo = rooms
 
     const rentalPrompt = (e) =>{
         // prompt("Confirm Rental by Entering Your Name")
         // use set State to change renter property to equal prompt input
-        console.log(e.target.);
+
+        // setting a rental would change the object insides of roooms that matches room.room and setting it's renter value to the input of that renter
+
+        const roomNum = e.target.value
+
+       demo.map( floor => {
+           return floor.map( room =>{
+               return console.log(room);
+           })
+       })
+
+       
+
+        console.log(roomNum);
     };
 
     return (
@@ -24,7 +41,7 @@ export default function Rent() {
                                 return(
                                     <li key={room.room}
                                         onMouseDown={rentalPrompt}
-                                       
+                                       value={room.room}
                                     >
                                        Room {room.room} ${room.price}
                                     </li>
