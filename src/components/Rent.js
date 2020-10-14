07cb106d8020cf2011efc546863415be053bd751
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-13 23:11:37 -0400

    set instance of rooms, add prompt, set renter to prompt value, setRooms to modified new instance of rooms

--- src/components/Rent.js
+++ src/components/Rent.js
@@ -3,11 +3,8 @@ import {RoomContext} from "../contexts/RoomContext"
 
 export default function Rent() {
 
-    const {rooms} = useContext(RoomContext);
+    const {rooms, setRooms} = useContext(RoomContext);
     const [name,setName] = useState("");
-
-
-    const demo = rooms
 
     const rentalPrompt = (e) =>{
         // prompt("Confirm Rental by Entering Your Name")
@@ -15,17 +12,27 @@ export default function Rent() {
 
         // setting a rental would change the object insides of roooms that matches room.room and setting it's renter value to the input of that renter
 
+        const demo = rooms
+
         const roomNum = e.target.value
 
-       demo.map( floor => {
+        const renter = prompt("Enter Your Name");
+
+        demo.map( floor => {
            return floor.map( room =>{
-               return console.log(room);
+
+            if(room.room == roomNum){
+                console.log(room);
+
+                room.renter = renter   
+            }
+            return room               
            })
-       })
+        })
 
+        setRooms(demo)
        
-
-        console.log(roomNum);
+        console.log(rooms);
     };
 
     return (
