2b0d4b8686531206cb8693218fae3136389ffcbd
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-15 20:08:36 -0400

    add returnPrompt method

--- src/components/Return.js
+++ src/components/Return.js
@@ -5,20 +5,48 @@ export default function Return() {
 
     const {rooms,setRooms} = useContext(RoomContext);
 
+    const returnPrompt = (e) =>{
+        const reRoom = prompt("Enter Renter's Name to Return Room");
+
+        console.log(reRoom);
+
+        const demo = rooms.map( floor =>{
+
+            return floor.map( room =>{
+
+                if(reRoom === room.renter){
+                    console.log(room);
+                    room.renter = null;
+                }
+
+                return room
+            })
+        })
+
+        setRooms(demo)
+    }
+
     
 
     return (
         <div className="Return">
+
             <ul className="Rented">
+
                {rooms.map( floor =>{
                    return floor.map( room =>{
+
                        if (room.renter !== null){
                            return(
-                               <li key={room.room} value={room.room}>
+                               <li key={room.room}
+                                onClick={returnPrompt}
+                               >
                                    {room.room} Rented
                                </li>
                            )
                        }
+                       else return null
+                      
                    })
                }
 
