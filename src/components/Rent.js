af2e89f4a8f0a071fc8f52790b298d490737526d
Jeremy Parker <jeremyeparker@yahoo.com>
2020-10-15 20:05:09 -0400

    change demo to rooms.map variable

--- src/components/Rent.js
+++ src/components/Rent.js
@@ -1,30 +1,28 @@
-import React,{useContext, useState} from 'react' 
+import React,{useContext} from 'react' 
 import {RoomContext} from "../contexts/RoomContext"
 
 export default function Rent() {
 
     const {rooms, setRooms} = useContext(RoomContext);
-    const [name,setName] = useState("");
-
     const rentalPrompt = (e) =>{
         // prompt("Confirm Rental by Entering Your Name")
         // use set State to change renter property to equal prompt input
 
         // setting a rental would change the object insides of roooms that matches room.room and setting it's renter value to the input of that renter
 
-        const demo = rooms
-
-        const roomNum = e.target.value
+        const roomNum = e.target.value;
 
         const renter = prompt("Enter Your Name");
 
-        demo.map( floor => {
+        
+
+        const demo = rooms.map( floor => {
            return floor.map( room =>{
 
-            if(room.room == roomNum){
-                console.log(room);
+            if(parseInt(room.room) === roomNum){
+                console.log(typeof(room.room));
 
-                room.renter = renter   
+                room.renter = renter;
             }
             return room               
            })
