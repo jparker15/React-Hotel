690cf9963c6df8935194bd08a927953c0c0cb9b4
Jar <jeremyeparker@yahoo.com>
2020-10-17 23:36:01 -0400

    hideRent method changes Rent display based on bool

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,23 @@ import Return from "./Return"
 
 export default function NavBar() {
 
+    let show = true; 
+
     const hideRent = (e) =>{
         console.log(e.target);
+
+        show = !show
+        console.log(show);
         
         const rentDiv = document.getElementsByClassName("Rent");
 
-        rentDiv[0].style.display = "none"
+        if(!show){
+            rentDiv[0].style.display = "none"
+        }
+        else{
+            rentDiv[0].style.display ="inline"
+        }
+
         console.log();
     }
 
